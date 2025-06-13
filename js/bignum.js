e17db51c4e0f5d37858b4a7be4a4f2783dcde503
pathanzaki <159039350+pathanzaki@users.noreply.github.com>
2025-06-13 17:07:11 +0530

    Add files via upload

---
+++ js/bignum.js
@@ -0,0 +1,9 @@
+let a = 10, b = 25, c = 55;
+
+if (a > b && a > c) {
+  console.log("Largest is", a);
+} else if (b > c) {
+  console.log("Largest is", b);
+} else {
+  console.log("Largest is", c);
+}
---
+++ js/fact.js
@@ -0,0 +1,8 @@
+let n = 6;
+let fact = 1;
+
+for (let i = 1; i <= n; i++) {
+  fact *= i;
+}
+
+console.log("Factorial =", fact);
